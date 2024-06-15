1d8b95b5941c8ba17533c449178320cbe9bf2314
zb <8761804@iesplayamar.es>
2024-06-15 05:00:31 +0200

    Añadido proxy inverso Nginx

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true
 };
 
-export const URL_BASE_BACKEND = 'http://143.47.48.255:8080/api';
+export const URL_BASE_BACKEND = 'http://143.47.48.255:8081/api';
 export const URL_API_BACKEND = `${URL_BASE_BACKEND}/users`;
